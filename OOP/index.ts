25923d489a28a2fb1b2ec1f40068575903ae7a35
olusegun <olusegunbamgbelu755@gmail.com>
2024-05-21 21:02:22 +0100

    learn about encapsulation in oop

--- OOP/index.ts
+++ OOP/index.ts
@@ -1,1 +1,24 @@
-console.log(2);
+class Player {
+    name: string
+    health: number
+    isInvincible: boolean
+    crush: Player;
+    greet() {
+        console.log(`Hello World, my name is ${this.name}`);
+        
+    }
+}
+
+const mario = new Player();
+mario.name = "Mario"
+mario.health = 10;
+mario.isInvincible = false;
+
+const peach = new Player();
+peach.name = "Peach";
+peach.health = 11;
+peach.isInvincible = true;
+
+mario.greet();
+peach.greet();
+
---
+++ OOP/notes.md
@@ -0,0 +1,7 @@
+# Learning OOP with TypeScript
+Classes are not objects but are used to construct objects
+
+### index.ts
+I learnt that variables are actually pointers!
+
+### encapsulation.ts
