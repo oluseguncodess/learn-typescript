37b086299e772ffd6576cdd439d88ad9ac6bb47a
olusegun <olusegunbamgbelu755@gmail.com>
2024-05-21 20:33:27 +0100

    made some changes

--- OOP/index.ts
+++ OOP/index.ts
@@ -1,13 +1,1 @@
-class Player {
-    health: number
-    speed: number
-    isInvincible: true;
-    move() {
-        console.log("Balll!!!");
-        
-    }
-}
-
-const mario = new Player();
-mario.health = 100;
-console.log(mario);
+console.log(2);
--- OOP/notes.md
+++
@@ -1,4 +0,0 @@
-## Learning Object Oriented Programming with TypeScript
-
-## What's a class, a superclass and a sub-class? 
-A class is not an object but is used to construct objects
